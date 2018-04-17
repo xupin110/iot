4884f51c91657ae23a248a6c361d6a743a52daad
xiaod <97404667@qq.com>
2018-04-17 01:51:49 -0400

    df

--- centerserver/device.php
+++ centerserver/device.php
@@ -7,7 +7,7 @@ class Device
     protected $client;
     private $data = [
             "DeviceSn" => "127.0.0.1",
-            "RequestControl" => "1",
+            "RequestControl" => "4",
             "Relay" => [
                 "1" => "0",
                 "2" => "0",
@@ -40,7 +40,7 @@ class Device
 
         //发起连接
         // $this->client->connect('47.95.220.109', 8090, 30);
-        $this->client->connect('127.0.0.1', 8901, 30);
+        $this->client->connect('127.0.0.1', 8090, 30);
         // print_r($this->client);
 
     }
